29c99a330f98696612a40879bf99b112cccaf60a
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-03-20 20:08:53 -0300

    update

--- Prog1/tentando dnv.py
+++
@@ -1,9 +0,0 @@
-import random
-baralho = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
-
-def BubbleSort(baralho):
-    embaralhado = random.shuffle(baralho)
-    for i in range(len(baralho)):
-        print(embaralhado)
-
-BubbleSort(baralho)
