030b4350e46cd8fe7fd42e091ab5918dec6d4b50
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-04-16 11:13:52 -0300

    add exercises

--- Beecrowd/BC1036.py
+++ Beecrowd/BC1036.py
@@ -1,38 +1,13 @@
-#AINDA NÃO RESOLVIDO
+import math
 
-# Função
-def bhaskara(entrada):
-    #Coversão
-    entrada = entrada.split()
+A, B, C = map(float, input().split())
+delta = B**2 - 4*A*C
+
+if delta >= 0 and A != 0:
+    raiz1 = (-B + math.sqrt(delta)) / (2*A)
+    raiz2 = (-B - math.sqrt(delta)) / (2*A)
     
-    #Declarando Variáveis
-    A = float(entrada[0])
-    B = float(entrada[1])
-    C = float(entrada[2])
-    
-    delta = B ** 2 
-    delta1 = delta * (-1 * (4 * A * C))
-    if A != 0 and delta1 > 0:
-        eq1 = (- B + (delta1 ** 0.5)) / 2 * A
-        eq2 = (- B - (delta1 ** 0.5)) / 2 * A
-
-        # CORREÇÃO
-        eq1 = eq1 / 100
-        eq2 = eq2 / 100
-
-        
-        # METODO 1
-        # print ("R1 = {:.5f}".format(eq1))
-        # print ("R2 = {:.5f}".format(eq2))
-
-        # METODO 2      
-        print ("R1 = {:.5f}".format(eq1))
-        print ("R2 = {:.5f}".format(eq2))
-
-    else:
-        print ("Impossivel calcular")
-
-
-# Código Principal
-entrada=input()
-bhaskara(entrada)
+    print("R1 = {:.5f}".format(raiz1))
+    print("R2 = {:.5f}".format(raiz2))
+else:
+    print("Impossivel calcular")
