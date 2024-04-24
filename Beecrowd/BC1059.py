539984e69d92c7e092e8f3a66723609aacde727e
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-04-24 09:44:59 -0300

    add exercises

--- Beecrowd/BC1059.py
+++ Beecrowd/BC1059.py
@@ -1,2 +1,26 @@
-for i in range (2, 102, 2):
-    print(i)
+# Não Resolvido
+def SomaImpar():
+    Inteiro1 = int(input())
+    Inteiro2 = int(input())
+    Saida = 0
+    
+    if Inteiro1 == Inteiro2:
+        print(0)
+    else:   
+        if Inteiro1 % 2 != 0:
+            if Inteiro1 > Inteiro2:
+                for i in range(Inteiro1, Inteiro2, 2):
+                    Saida += i
+            else:
+                for i in range(Inteiro1, Inteiro2, -2):
+                    Saida += i
+                
+        else:
+            if Inteiro2 > 0:
+                for i in range(Inteiro1, Inteiro2+1, 2):
+                    Saida += i
+            else:
+                for i in range(Inteiro1, Inteiro2+1, -2):
+                    Saida += i   
+    print(Saida)  
+SomaImpar()
