07aedb5760b648ad665fbca9e80f574e97d69988
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-05-08 10:58:36 -0300

    add exercises

--- Beecrowd/BC2296.py
+++ Beecrowd/BC2296.py
@@ -1,24 +1,23 @@
-def calcular_esforco_subida(altitudes):
-    esforco = 0
-    for i in range(1, len(altitudes)):
-        if altitudes[i] > altitudes[i - 1]:
-            esforco += altitudes[i] - altitudes[i - 1]
-    return esforco
+import sys
+#ler quantidade N de valores
+Numero_de_trilhas = int(input())
 
-N = int(input())
+#sentinela
+custo = sys.maxsize 
+indice_de_menor_custo = None
 
-melhor_trilha = 1
-menor_esforco = float('inf')
+#ler valores da trilha1, ou seja, M e Hi
+for i in range(1, Numero_de_trilhas-1):
+    lista_de_valores = list(map(int, input().split()))
+    
+    
 
-for trilha_id in range(1, N + 1):
-    descricao = input().split()
-    M = int(descricao[0])
-    altitudes = list(map(int, descricao[1:]))
 
-    esforco = calcular_esforco_subida(altitudes)
+#Calcular custo de ida (CIda) da T1 
+#Calcular custo de volta (CVolta) da T1 
+#Calcular o custo da T1, sendo esse o menor entre Cida e Cvolta
+#Atualizar a anotação de menor custo, caso CustoT1 seja menor que o menor custo anotado
 
-    if esforco < menor_esforco or (esforco == menor_esforco and trilha_id < melhor_trilha):
-        melhor_trilha = trilha_id
-        menor_esforco = esforco
-
-print(melhor_trilha)
+#repetir o processo para a T2
+#repetir o processo para a T3
+#repetir o processo até Tn
