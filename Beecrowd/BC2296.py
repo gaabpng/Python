c53372dec5622c473c68e36327c78567e1fa1342
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-05-08 11:02:07 -0300

    add exercises

--- Beecrowd/BC2296.py
+++ Beecrowd/BC2296.py
@@ -9,14 +9,14 @@ indice_de_menor_custo = None
 #ler valores da trilha1, ou seja, M e Hi
 for i in range(1, Numero_de_trilhas-1):
     lista_de_valores = list(map(int, input().split()))
-    
-    
 
-
-#Calcular custo de ida (CIda) da T1 
-#Calcular custo de volta (CVolta) da T1 
-#Calcular o custo da T1, sendo esse o menor entre Cida e Cvolta
-#Atualizar a anotação de menor custo, caso CustoT1 seja menor que o menor custo anotado
+    #Calcular custo de ida (CIda) da T1 
+    CIda = 0
+    #Calcular custo de volta (CVolta) da T1 
+    CVolta = 0 
+    #Calcular o custo da T1, sendo esse o menor entre Cida e Cvolta
+    custo_trilha = min(CIda, CVolta)
+    #Atualizar a anotação de menor custo, caso CustoT1 seja menor que o menor custo anotado
 
 #repetir o processo para a T2
 #repetir o processo para a T3
