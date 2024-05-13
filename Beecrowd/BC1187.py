854acbc680ac01e6bcab81fd369ff3daaea21597
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-05-13 12:46:18 -0300

    correção na soma das listas

--- Beecrowd/BC1187.py
+++ Beecrowd/BC1187.py
@@ -1,34 +1,26 @@
 #Não Resolvido
+import random
+
 linhas = 12
 colunas = 12
 matriz = []
-
 op = input()
-for i in range(linhas):
-    linha = []
-    for j in range(colunas):
-        valor = float(input())
-        linha.append(valor)
-    matriz.append(linha)
+
+for i in range(colunas):
+    matriz.append([])
+    for j in range(linhas):
+        valor = random.randint(1, 100)
+        matriz[i].append(valor)
     
-linha1 = matriz[0][1:10]
-linha1 = sum(linha1)
+linha1 = sum(matriz[0][1:11])
+linha2 = sum(matriz[1][2:10])
+linha3 = sum(matriz[2][3:9])
+linha4 = sum(matriz[3][4:8])
+linha5 = sum(matriz[4][5:7])
 
-linha2 = matriz[1][2:9]
-linha2 = sum(linha2)
+resultado = linha1+linha2+linha3+linha4+linha5
 
-linha3 = matriz[2][3:8]
-linha3 = sum(linha3)
-
-linha4 = matriz[3][4:7]
-linha4 = sum(linha4)
-
-linha5 = matriz[4][5:6]
-linha5 = sum(linha5)
-
-resultado = float(linha1+linha2+linha3+linha4+linha5)
 if op == "S":
     print("{:.1f}".format(resultado))
 else:
-    resultado = resultado/30
     print("{:.1f}".format(resultado))
--- Prog1/Whiteboard.py
+++ Prog1/Whiteboard.py
@@ -1,3 +1,11 @@
-lista = [0, 1, 2, 3, 4, 5]
-lista = lista[::-1]
-print(lista)
+colunas = 4
+linhas = 4
+matriz = []
+
+for i in range(colunas):
+    matriz.append([])
+    for j in range(linhas):
+        inserção = int(input(f"Digite o valor da linha {i+1} e coluna {j+1}: "))
+        matriz[i].append(inserção)
+        
+print(matriz)
