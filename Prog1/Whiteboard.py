910899e67880b33813d9d224e5e3c839990dbefe
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-05-14 14:06:00 -0300

    whiteboard

--- Prog1/Whiteboard.py
+++ Prog1/Whiteboard.py
@@ -1,11 +1,36 @@
-colunas = 4
-linhas = 4
-matriz = []
+telefone = 0
+presente = 0
+moraPerto = 0
+devia = 0
+trabalhou = 0
 
-for i in range(colunas):
-    matriz.append([])
-    for j in range(linhas):
-        inserção = int(input(f"Digite o valor da linha {i+1} e coluna {j+1}: "))
-        matriz[i].append(inserção)
-        
-print(matriz)
+telefone = input("telefonou?")
+if telefone == "Sim":
+    telefone = 1
+    
+presente = input("deu presente?")
+if presente == "Sim":
+    presente = 1
+    
+moraPerto = input("mora perto?")
+if moraPerto == "Sim":
+    moraPerto = 1
+
+devia = input("devia?")
+if devia == "Sim":
+    devia = 1
+    
+trabalhou = input("trabalhou?")
+if trabalhou == "Sim":
+    trabalhou = 1
+
+if telefone + presente + moraPerto + devia + trabalhou <= 1:
+    print("Inocente")
+else:
+    if telefone + presente + moraPerto + devia + trabalhou == 2:
+        print("Suspeito")
+    else:
+        if telefone + presente + moraPerto + devia + trabalhou == 3 or telefone + presente + moraPerto + devia + trabalhou == 4:
+            print("Cúmplice")
+        else:
+            print("Assassino")
