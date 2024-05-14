941d139d547d1a869e10690ae5ff847bfe95d6af
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-05-14 14:05:26 -0300

    add exercise

--- Beecrowd/BC1187.py
+++ Beecrowd/BC1187.py
@@ -1,6 +1,3 @@
-#Não Resolvido
-import random
-
 linhas = 12
 colunas = 12
 matriz = []
@@ -9,7 +6,7 @@ op = input()
 for i in range(colunas):
     matriz.append([])
     for j in range(linhas):
-        valor = random.randint(1, 100)
+        valor = float(input())
         matriz[i].append(valor)
     
 linha1 = sum(matriz[0][1:11])
@@ -23,4 +20,5 @@ resultado = linha1+linha2+linha3+linha4+linha5
 if op == "S":
     print("{:.1f}".format(resultado))
 else:
+    resultado = resultado/30
     print("{:.1f}".format(resultado))
