abfff9404a724d505d20dea12cf11efad541a66e
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-05-17 23:21:12 -0300

    fix: criação da matriz para aquisição dos valores

--- Beecrowd/BC2296.py
+++ Beecrowd/BC2296.py
@@ -1,23 +1,9 @@
 import sys
-#ler quantidade N de valores
 Numero_de_trilhas = int(input())
+trilhas = []
+distância_das_trilhas = []
+menor_trilha = sys.maxsize 
+trilha_atual=0
 
-#sentinela
-custo = sys.maxsize 
-indice_de_menor_custo = None
-
-#ler valores da trilha1, ou seja, M e Hi
-for i in range(1, Numero_de_trilhas-1):
-    lista_de_valores = list(map(int, input().split()))
-
-    #Calcular custo de ida (CIda) da T1 
-    CIda = 0
-    #Calcular custo de volta (CVolta) da T1 
-    CVolta = 0 
-    #Calcular o custo da T1, sendo esse o menor entre Cida e Cvolta
-    custo_trilha = min(CIda, CVolta)
-    #Atualizar a anotação de menor custo, caso CustoT1 seja menor que o menor custo anotado
-
-#repetir o processo para a T2
-#repetir o processo para a T3
-#repetir o processo até Tn
+for i in range(Numero_de_trilhas):
+    trilhas.append(list(map(int, input().split())))
