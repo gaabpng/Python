56680e9da49de9275e170a338de091ba0babf43d
gaabpng <gabriel.s.rodrigues.ifrj@gmail.com>
2024-05-22 10:43:54 -0300

    feat: adição do exercício 2296

--- Beecrowd/BC2296.py
+++ Beecrowd/BC2296.py
@@ -1,9 +1,29 @@
 import sys
-Numero_de_trilhas = int(input())
-trilhas = []
-distância_das_trilhas = []
-menor_trilha = sys.maxsize 
-trilha_atual=0
+entrada = sys.stdin.read
+dados = entrada().strip().split('\n')
 
-for i in range(Numero_de_trilhas):
-    trilhas.append(list(map(int, input().split())))
+N = int(dados[0])
+menor_esforco = float('inf')
+melhor_trilha = -1
+
+for i in range(1, N + 1):
+    dados_trilha = list(map(int, dados[i].split()))
+    M = dados_trilha[0]
+    alturas = dados_trilha[1:]
+    
+    esforco_normal = 0
+    esforco_reverso = 0
+    
+    for j in range(1, M):
+        if alturas[j] > alturas[j - 1]:
+            esforco_normal += alturas[j] - alturas[j - 1]
+        if alturas[M - j - 1] > alturas[M - j]:
+            esforco_reverso += alturas[M - j - 1] - alturas[M - j]
+    
+    menor_esforco_trilha = min(esforco_normal, esforco_reverso)
+    
+    if menor_esforco_trilha < menor_esforco:
+        menor_esforco = menor_esforco_trilha
+        melhor_trilha = i
+
+print(melhor_trilha)
